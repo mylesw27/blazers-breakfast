56e17e34d93db2cc8e764c06d1688bfe485d68c4
mylesw27 <myleswiegel27@gmail.com>
2023-04-19 22:27:10 -0700

    Update header on landing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Blazer's Breakfast</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coming soon. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

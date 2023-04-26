ec423ba055185982901b3e2205b3b99f8a7bf57d
Blake Longfield <blakelongfield@gmail.com>
2023-04-25 20:10:10 -0700

    got local set up working

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,11 @@
-
+import React from 'react';
 
 export default function Navbar() {
     return(
         <nav class="navigation">
-        <h1 class="">Blazer's Breakfast</h1>
+        <h1 class="">
+            Blazer's Breakfast
+        </h1>
         </nav>
     )
 }

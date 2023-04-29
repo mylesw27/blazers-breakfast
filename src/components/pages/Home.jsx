1047630eb45f43cf557c0600b0ca7f17a384205e
mylesw27 <myleswiegel27@gmail.com>
2023-04-29 02:19:06 -0700

    Add picture carousel BB-8

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Logo from "../partials/Logo"
 import MenuNavigation from "../menu/MenuNavigation"
+import Carousel from "../partials/Carousel"
 
 export default function () {
     return (
         <div>
             <h1>Home</h1>
             <Logo />
+            <Carousel />
             <MenuNavigation />
         </div>
     )
